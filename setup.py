3d584aa6179c06be3257c05417d1afe66222eb25
PamuriLakshmanReddy <lakshman2905@gmail.com>
2025-03-04 19:30:36 +0530

    folder structer added

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages,setup
+setup(name='NLP_Chatbot',
+      version='0.0.0',
+      author='Lakshman',
+      author_email='lakshman2905@gmail.com',
+      packages=find_packages(),
+      install_requires=[])
